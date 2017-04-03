f0159588ae44b4df48f2387f1321414ccbc9f938
guillaumew <geeyom12@gmail.com>
2017-04-03 17:16:30 +0200

    new landing page

--- app/welcome/welcome.component.js
+++ app/welcome/welcome.component.js
@@ -7,18 +7,6 @@ angular.
         Analytics.pageView();
         // FEATURES
         var self = this;
-        self.openApp = function openApp(){
-          if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement) {  // current working methods
-            if (document.documentElement.requestFullscreen) {
-              document.documentElement.requestFullscreen();
-            } else if (document.documentElement.mozRequestFullScreen) {
-              document.documentElement.mozRequestFullScreen();
-            } else if (document.documentElement.webkitRequestFullscreen) {
-              document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-            }
-          }
-          $location.path('/paths/');
-        }
       }
     ]
   });
