012e9beaa74360d02823c243a4ccf808ab4291bf
guillaumew <geeyom12@gmail.com>
2017-05-12 17:31:53 +0200

    safer apiurl switch

--- app/core/apiurl/apiurl.service.js
+++ app/core/apiurl/apiurl.service.js
@@ -2,8 +2,13 @@ angular.
 	module('core.apiurl').
 	factory('Apiurl', ['$location',
 	function ($location) {
+		var host = $location.protocol() + "://" + $location.host() + ":" + $location.port();
+		
+		if ($location.host().indexOf("localhost")>-1){
+			host="https://oye-app.herokuapp.com";
+		}
+
 		return {
-			host: $location.protocol() + "://" + $location.host() + ":" + $location.port()
-			//host: "https://oye-app.herokuapp.com"
+			host: host
 		}; 
 	}]);
